b3ccadcc0f2fffc18e5d4ec9e3ae875b200184bb
Renzo Torres <renzo.torres@wolox.com.ar>
2019-12-30 17:06:31 -0300

    Change concat for spread operator

--- react/tic-tac-toe/src/app/screens/Game/index.js
+++ react/tic-tac-toe/src/app/screens/Game/index.js
@@ -14,19 +14,19 @@ class Game extends React.Component {
   }
   
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
+    const { history, xIsNext, stepNumber} = this.state;
+
+    const currHistory = history.slice(0, stepNumber + 1);
+    const current = currHistory[currHistory.length - 1];
     const squares = current.squares.slice();
     
     if (calculateWinner(squares) || squares[i]) return;
     
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
-      history: history.concat([{
-        squares: squares
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext
+      history: [...currHistory, {squares}],
+      stepNumber: currHistory.length,
+      xIsNext: !xIsNext
     });
   }
 
