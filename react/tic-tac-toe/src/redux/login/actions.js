86df1c4550d949e6d52147fe929fbc3fa17faaa2
Renzo Torres <renzo.torres@wolox.com.ar>
2020-01-09 11:48:30 -0300

    Token saved on app store

--- react/tic-tac-toe/src/redux/login/actions.js
+++ react/tic-tac-toe/src/redux/login/actions.js
@@ -3,8 +3,17 @@ import loginService from '../../services/LoginService';
 const actionCreators = {
 	loginUser: (email, password) => async dispatch => {
 		const response = await loginService.login(email, password);
-		console.log(`Token: ${response.data.token}`);
-	}	
+		if (response.ok) {
+			dispatch(actionCreators.saveToken(response.data.token));
+		} else {
+			console.log(response.data);
+			alert('Incorrect Password');
+		}
+	},
+	saveToken: token => ({
+		type: 'SAVE_TOKEN',
+		token
+	})
 }
 
 export default actionCreators;
--- react/tic-tac-toe/src/redux/login/reducer.js
+++ react/tic-tac-toe/src/redux/login/reducer.js
@@ -1,7 +1,14 @@
-const initialState = {};
+const initialState = {
+	token: undefined
+};
 
 function reducer(state = initialState, action){
 	switch(action.type) {
+		case 'SAVE_TOKEN':
+			return {
+				...state,
+				token: action.token
+			}
 		default:
 			return state;
 	}
