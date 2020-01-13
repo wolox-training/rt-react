0104ffb7475f4c5d9c699186e66f9eb2d4256556
Renzo Torres <renzo.torres@wolox.com.ar>
2020-01-13 20:38:23 -0300

    Constants added to profile view

--- react/tic-tac-toe/src/app/screens/Profile/index.js
+++ react/tic-tac-toe/src/app/screens/Profile/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import styles from './styles.module.scss';
-import { BACK_LABEL } from './constants';
+import { LABELS } from './constants';
 
 import actionCreators from '~redux/login/actions';
 
@@ -17,11 +17,11 @@ class Profile extends Component {
             <img className={styles.img} src={profileImg} />
           </div>
           <div className={styles.descContainer}>
-            <h1 className={styles.title}>User Profile</h1>
+            <h1 className={styles.title}>{LABELS.title}</h1>
             <div className={styles.data}>
-              <p className={styles.dataUser}>Name: <span>Renzo Torres</span></p><br/>
-              <p className={styles.dataUser}>Age: <span>31</span></p><br/>
-              <p className={styles.dataUser}>Address: <span>San Luis - 1234</span></p>
+              <p className={styles.dataUser}>{LABELS.name}: <span>Renzo Torres</span></p><br/>
+              <p className={styles.dataUser}>{LABELS.age}: <span>31</span></p><br/>
+              <p className={styles.dataUser}>{LABELS.address}: <span>San Luis - 1234</span></p>
             </div>
             <div>
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
@@ -29,7 +29,7 @@ class Profile extends Component {
           </div>
         </div>
         <a className={styles.linkBack} href="#" onClick={this.props.goBackPage}>
-          {BACK_LABEL}
+          {LABELS.back}
         </a>
       </main>
     )
