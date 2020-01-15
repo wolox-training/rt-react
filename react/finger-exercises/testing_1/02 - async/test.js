83c2409c09f4177631b53cd50b272636f823b716
Renzo Torres <renzo.torres@wolox.com.ar>
2020-01-15 12:58:25 -0300

    Async - getData resolves the data if true is sent as argument (use async/await)

--- react/finger-exercises/testing_1/02 - async/test.js
+++ react/finger-exercises/testing_1/02 - async/test.js
@@ -1,8 +1,9 @@
 import getData from '.';
 
 describe('02 - async', () => {
-  xit('getData resolves the data if true is sent as argument (use async/await)', async () => {
-    getData();
+  it('getData resolves the data if true is sent as argument (use async/await)', async () => {
+    const data = await getData(true);
+    expect(data).toBe('data');
   });
   xit('getData resolves the data if true is sent as argument (avoid async/await)', () => {
   });
