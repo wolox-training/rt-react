a2c95e40096ba1ab65de59fcc5b16f02557055a9
Renzo Torres <renzo.torres@wolox.com.ar>
2020-01-15 18:02:52 -0300

    Enzyme - dashboard tests - wip

--- react/finger-exercises/testing_2/src/screens/Dashboard/index.test.js
+++ react/finger-exercises/testing_2/src/screens/Dashboard/index.test.js
@@ -9,11 +9,13 @@ import Dashboard from './index';
 configure({ adapter: new Adapter() });
 
 describe('Dashboard', () => {
-  xit('first h1 text should be "Welcome to React"', () => {
+  it('first h1 text should be "Welcome to React"', () => {
     const wrapper = shallow(<Dashboard />);
+    expect(wrapper.find('h1').first().text()).toBe("Welcome to React");
   });
-  xit('parent of the logo should be a header', () => {
+  it('parent of the logo should be a header', () => {
     const wrapper = shallow(<Dashboard />);
+    expect(wrapper.find('.App-logo').parent().is('header')).toBe(true);
   });
   xit('componentDidMount is called once', () => {
   })
