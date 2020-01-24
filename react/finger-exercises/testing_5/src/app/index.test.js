e36ff1dbefe9d932d93a3adf7a500e851f7e469b
Renzo Torres <renzo.torres@wolox.com.ar>
2020-01-24 18:59:32 -0300

    Text validations tests

--- react/finger-exercises/testing_5/src/app/index.test.js
+++ react/finger-exercises/testing_5/src/app/index.test.js
@@ -8,6 +8,15 @@ describe('H1 Text', () => {
     async () => {
       const browser = await Puppeteer.launch();
       const page = await browser.newPage();
+
+      await page.goto(TEST_BASE_URL);
+      const h1Title = await page.$eval('.app-title', e => e.innerHTML);
+      expect(h1Title).toBe('Welcome to React');
+
+      const h1SubTitle = await page.$eval('.app-subtitle', e => e.innerHTML);
+      expect(h1SubTitle).toBe('This is a finger exercise');
+
+      browser.close();
     },
     16000
   );
