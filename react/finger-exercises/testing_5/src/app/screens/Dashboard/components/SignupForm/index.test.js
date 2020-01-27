e72235619d35b072f2f42569fc26d862ac3860d3
Renzo Torres <renzo.torres@wolox.com.ar>
2020-01-27 15:40:53 -0300

    SignupForm tests

--- react/finger-exercises/testing_5/src/app/screens/Dashboard/components/SignupForm/index.test.js
+++ react/finger-exercises/testing_5/src/app/screens/Dashboard/components/SignupForm/index.test.js
@@ -15,21 +15,84 @@ describe('Signup Form (use FIELDS constants)', () => {
   let browser;
   let page;
 
-  beforeAll(async () => {}); // start the browser
+  beforeAll(async () => {
+    browser = await Puppeteer.launch();
+  }); // start the browser
 
-  beforeEach(async () => {}); // go to root page
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto(TEST_BASE_URL);
+  }); // go to root page
 
-  afterAll(() => {}); // close browesr
+  it('Can submit signup form', async () => {
+    await page.waitForSelector('.signup-form');
+    await page.click('input[name=firstName]');
+    await page.type('input[name=firstName]', 'Renzo');
+    await page.click('input[name=lastName]');
+    await page.type('input[name=lastName]', 'Torres');
+    await page.click('input[name=email]');
+    await page.type('input[name=email]', 'renzo.torres@wolox.com.ar');
+    await page.click('button[type=submit]');
+    await page.waitForSelector('span.result');
+  });
 
-  it('Can submit signup form', async () => {});
+  it(
+    'Successful submit',
+    async () => {
+      await page.waitForSelector('.signup-form');
+      await page.click('input[name=firstName]');
+      await page.type('input[name=firstName]', 'Renzo');
+      await page.click('input[name=lastName]');
+      await page.type('input[name=lastName]', 'Torres');
+      await page.click('input[name=email]');
+      await page.type('input[name=email]', 'renzo.torres@wolox.com.ar');
+      await page.click('button[type=submit]');
+      await page.waitForFunction(
+        'document.querySelector("body").innerText.includes("User has been created successfully")'
+      );
+    },
+    16000
+  );
 
-  it('Successful submit', async () => {}, 16000);
+  it(
+    '3 fields are required',
+    async () => {
+      await page.click('button[type=submit]');
+      await page.waitForSelector('span.error-firstName');
+      await page.waitForSelector('span.error-lastName');
+      await page.waitForSelector('span.error-email');
+    },
+    16000
+  );
 
-  it('3 fields are required', async () => {}, 16000);
+  it(
+    'First name is required',
+    async () => {
+      await page.click('button[type=submit]');
+      await page.waitForSelector('span.error-firstName');
+    },
+    16000
+  );
 
-  it('First name is required', async () => {}, 16000);
+  it(
+    'Last name is required',
+    async () => {
+      await page.click('button[type=submit]');
+      await page.waitForSelector('span.error-lastName');
+    },
+    16000
+  );
 
-  it('Last name is required', async () => {}, 16000);
+  it(
+    'Email is required',
+    async () => {
+      await page.click('button[type=submit]');
+      await page.waitForSelector('span.error-email');
+    },
+    16000
+  );
 
-  it('Email is required', async () => {}, 16000);
+  afterAll(() => {
+    browser.close();
+  }); // close browesr
 });
