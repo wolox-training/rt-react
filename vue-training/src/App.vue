cf0db17e30398783f0b787207efc2b73e75ae597
Renzo Torres <renzo.torres@wolox.com.ar>
2021-06-24 10:09:16 -0300

    Fix comments to add project pr

--- vue-training/src/App.vue
+++ vue-training/src/App.vue
@@ -1,31 +1,33 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link class="nav-link" to="/">Home</router-link> |
+      <router-link class="nav-link" to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import "./scss/_colors.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $pickled-bluewood;
 }
 
 #nav {
   padding: 30px;
 
-  a {
+  .nav-link {
     font-weight: bold;
-    color: #2c3e50;
+    color: $pickled-bluewood;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: $ocean-green;
     }
   }
 }
