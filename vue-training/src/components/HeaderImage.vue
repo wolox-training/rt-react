45ec93a1cb200e4d07c91adeee3e39195b3fb906
Renzo Torres <renzo.torres@wolox.com.ar>
2021-06-25 16:48:20 -0300

    Login - view and request

---
+++ vue-training/src/components/HeaderImage.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  img.header-image(alt='Wolox logo' src='@/assets/header-image.png')
+</template>
+
+<script>
+export default {
+  name: 'HeaderImage'
+}
+</script>
+
+<style scoped lang="scss">
+  .header-image {
+    margin-bottom: 40px;
+    width: 100%
+  }
+</style>
--- vue-training/src/views/Login.vue
+++ vue-training/src/views/Login.vue
@@ -1,11 +1,118 @@
 <template lang="pug">
   .main-container
-    h1
-      | Login view
+    header-image
+    .form-container
+      form
+        form-input(
+          v-for='(input, key) in inputs'
+          :key='key'
+          :input='input'
+          v-model='input.value'
+          :validator='$v.inputs[key]'
+        )
+        form-button(:button='loginBtn' @handle-click='handleLogin')
+        hr.buttons-line
+        form-button(:button='signUpBtn' @handle-click='handleSignUp')
 </template>
 
 <script>
+import { required, email } from 'vuelidate/lib/validators'
+import { sessionUser } from '@/services/UserService'
+import HeaderImage from '@/components/HeaderImage'
+import FormInput from '@/components/FormInput'
+import FormButton from '@/components/FormButton'
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  components: {
+    FormButton,
+    FormInput,
+    HeaderImage
+  },
+  data () {
+    return {
+      inputs: {
+        email: { label: 'Email', type: 'email', value: '' },
+        password: { label: 'Password', type: 'password', value: '' }
+      },
+      signUpBtn: {
+        text: 'Sign up',
+        class: 'secondary'
+      },
+      loginBtn: {
+        text: 'Login',
+        class: 'primary'
+      }
+    }
+  },
+  validations: {
+    inputs: {
+      email: {
+        value: { required, email }
+      },
+      password: {
+        value: {
+          required,
+          strongPass (value) {
+            return (
+              /[A-Z]/.test(value) &&
+              /[0-9]/.test(value)
+            )
+          }
+        }
+      }
+    }
+  },
+  methods: {
+    handleLogin () {
+      this.$v.$touch()
+      if (this.$v.$error) {
+        return
+      }
+
+      const user = {
+        session: {
+          email: this.inputs.email.value,
+          password: this.inputs.password.value
+        }
+      }
+
+      sessionUser(user)
+        .then(response => {
+          if (response.ok) {
+            console.log('[access-token]', response.data.access_token)
+          }
+        })
+    },
+    handleSignUp () {
+      this.$router.push({ name: 'register' })
+    }
+  }
 }
 </script>
+
+<style scoped lang="scss">
+  @import "@/scss/variables.scss";
+
+  .main-container {
+    align-items: center;
+    background-color: $white-smoke;
+    border-top: solid 4px $deep-sky-blue;
+    display: flex;
+    flex-direction: column;
+    font-family: sans-serif;
+    margin: 100px auto;
+    max-width: 300px;
+    padding: 30px 20px;
+    width: 100%;
+  }
+
+  .form-container {
+    width: 100%;
+  }
+
+  .buttons-line {
+    border: 1px solid $mercury;
+    margin: 20px 0;
+  }
+</style>
